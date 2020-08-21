d14ae6e736f6a0854725324d4a47afce248e3963
Allan Montilla <allangmr7@gmail.com>
2020-08-20 23:58:04 -0500

    Commit Inicial

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,21 @@
+console.log("Hola mundo");
+
+
+//Variables y Constantes
+
+const nombre = 'Allan';
+const apellido = 'Montilla';
+
+
+let valorDado = 4;
+valorDado = 5;
+
+console.log(nombre,apellido, valorDado);
+
+if(true){
+
+    let valorDado = 6;
+    let nombre = "Gerardo";
+    console.log(valorDado);
+    console.log(nombre);
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const nombre = 'Allan';
+const apellido = 'Montilla';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const nombreCompleto = ` 
+El nombre completo es:
+${nombre} ${apellido}.
+`;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(nombreCompleto);
+
